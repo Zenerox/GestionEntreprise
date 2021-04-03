dcf62d1625331fc0290c27b711050e1bdf5c32d2
Zenerox <31919012+Zenerox@users.noreply.github.com>
2021-04-03 17:40:18 +0200

    MAJ nav + RdvController pour affichage template

--- src/Controller/RdvController.php
+++ src/Controller/RdvController.php
@@ -6,13 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/rdv')]
 class RdvController extends AbstractController
 {
-    #[Route('/rdv', name: 'app_list_rdv')]
-    public function index(): Response
+    #[Route('/planning', name: 'app_planning_rdv')]
+    public function planning(): Response
     {
-        return $this->render('rdv/list.html.twig', [
+        return $this->render('rdv/planning.html.twig', [
             'controller_name' => 'RdvController',
         ]);
     }
+
 }
